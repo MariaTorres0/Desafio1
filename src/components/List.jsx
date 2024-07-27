6da0adcefb36caded61541e3893682c81574e451
MariaTorres0 <mariatorres11001@gmail.com>
2024-07-27 11:57:33 -0600

    modificando los archivos y cambiando a otro json

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -4,12 +4,12 @@ import { data } from "@/app/data";
 export const List = () => {
     return (
         <div className="container p-4">
-            <hr />
-            <h1 className="text-center">Crisis económicas</h1>
-            <hr />
             {data.map((item, index) => (
                 <div className="row" key={index}>
-                    {item.crisis_economicas.map((value, id) => (
+                    <hr />
+                    <h1 className="text-center">{item.descripcion}</h1>
+                    <hr />
+                    {item.empresas.map((value, id) => (
                         <div className="col-md-6 col-lg-4 mb-4" key={id}>
                             <div className="card h-100">
                                 <div className="card-body d-flex flex-column">
@@ -20,18 +20,16 @@ export const List = () => {
                                             <p className="card-text">{value.descripcion}</p>
                                         </div>
                                         <div className="col">
-                                            <img src={value.imagen_de_referencia} className="img-fluid" alt={value.titulo} />
+                                            <img src={value.imagen} className="img-fluid" alt={value.titulo} />
                                         </div>
                                     </div>
                                     <div className="row">
                                         <div className="col pt-3">
                                             <h6 className="card-subtitle mb-2 fw-bold">Características:</h6>
                                             <ul className="list-unstyled">
-                                                {Object.entries(value.caracteristicas).map(([key, caracteristica], idx) => (
-                                                    <li key={idx} className="mb-1">
-                                                        <strong>{key}:</strong> {caracteristica}
-                                                    </li>
-                                                ))}
+                                                    <li className="mb-1">
+                                                        {value.caracteristicas}
+                                                    </li>                                           
                                             </ul>
                                         </div>
                                     </div>
